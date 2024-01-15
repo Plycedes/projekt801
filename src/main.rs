a5d71ff6b2fa7680c62ce4f5675f8ec69a68a3a2
Plycedes <abhaypratapyadav340@gmail.com>
2024-01-15 13:02:56 +0530

    Project Upload

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+mod user;
+mod sum;
+use std::io;
+
+fn main() {
+    let f: [&str; 2] = ["sum", "user"];
+
+    println!("Enter the name of the operation you want to perform!");
+    println!("Enter ls to list all the operations!");
+    println!("Enter exit to terminate the program");
+
+    let mut exec = true;
+
+    while exec {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Error");        
+
+        if input.trim() == "ls" {
+            println!("------------");
+            println!("OPERATIONS:");
+            let mut i = 0;
+            while i != 2{
+                println!("{}",f[i]);
+                i += 1;
+            }
+            println!("------------");
+        }
+        else if input.trim() == "exit" {
+            exec = false;
+        }
+        else if input.trim() == "sum"{
+            sum::sum();
+        }
+        else if input.trim() == "user"{
+            user::name();
+        }
+        else {
+            println!("Enter a valid input!!");
+        }
+
+    }    
+    println!("Terminated");
+}   
---
+++ src/sum.rs
@@ -0,0 +1,15 @@
+use std::io;
+
+pub fn sum(){
+	println!("Enter two numbers");
+	//test
+	
+	let mut s = String::new();
+	io::stdin().read_line(&mut s).expect("Error");
+	let x: u32 = s.trim().parse().unwrap();
+
+	s = String::new();
+	io::stdin().read_line(&mut s).expect("Error");
+	let y: u32 = s.trim().parse().unwrap();
+	println!("Sum = {}", x + y);
+}
